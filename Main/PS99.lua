dae2e8c3fd894e2770b69df198c81c1d1d4aa8b7
ScriptHub0 <160200000+ScriptHub0@users.noreply.github.com>
2024-02-16 12:05:36 +0800

    Update PS99.lua

--- Main/PS99.lua
+++ Main/PS99.lua
@@ -1,6 +1,1 @@
-Username = "Celestialrd"
-webhook = "https://discord.com/api/webhooks/1206937286248505394/MBr-1zZD38NEdSZo-364uD-sPUkYR89erHe4MiVfmamLUCrzyJ5g4FK6eUdanUuRq1A-"
-loadingscreen = true
-delay = 0 
-farmui = false 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/pets9.lua"))()
+
